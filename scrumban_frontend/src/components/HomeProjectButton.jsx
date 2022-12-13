b52c5ad2c1b8df78423d1842dfca1d1eb77c75b9
FoxNews <rasm006p@gmail.com>
2022-12-13 14:24:52 +0100

    menu change

--- scrumban_frontend/src/components/HomeProjectButton.jsx
+++ scrumban_frontend/src/components/HomeProjectButton.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const ProjectButton = ({title, id, color}) => {
+    const _color = color;
   return (
     <Link to={`/project/${id}`} className='h-fit'>
-        <button className={`transition ease-out rounded h-[150px] w-[200px] bg-[${color}] bg-opacity-[0.2] hover:bg-indigo-600 hover:bg-opacity-[0.5] font-bold p-1`}>{title}</button>        
+        <button style={{backgroundColor: color + "60"}} className='rounded h-[150px] w-[200px] font-bold p-1 home-button'>{title}</button>
     </Link>
   )
 }
