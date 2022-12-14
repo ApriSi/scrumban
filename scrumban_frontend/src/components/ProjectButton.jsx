02e1be2f879a05766c25150230686ada2f1d7e8d
FoxNews <rasm006p@gmail.com>
2022-12-14 14:05:48 +0100

    update

--- scrumban_frontend/src/components/ProjectButton.jsx
+++ scrumban_frontend/src/components/ProjectButton.jsx
@@ -4,7 +4,10 @@ import { Link } from 'react-router-dom'
 const ProjectButton = ({title, id, color}) => {
   return (
     <Link to={`/project/${id}`} className='h-fit'>
-        <button className={`rounded bg-color${color} hover:bg-indigo-600/[0.8] font-bold p-1 w-full`}>{title}</button>
+        <button className={`rounded bg-white/[0.1] hover:bg-indigo-600/[0.8] font-bold w-full relative`}>
+          <div className={`absolute bottom-0 w-[100%] h-[3px] bg-color${color} rounded`}/>
+          <p className='p-1'>{title}</p>
+        </button>
     </Link>
   )
 }
